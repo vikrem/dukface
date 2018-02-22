64ee15437ec52c9d32c5906a48da04436bc49c10
Vikrem <me@vikrem.com>
2018-02-22 15:04:22 -0500

    inline-c up

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Lib
     ( someFunc
     ) where
 
+import qualified Language.C.Inline as C
+
+C.include "duktape.h"
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = print =<< [C.block| int {
+                         duk_context *ctx = duk_create_heap_default();
+                         duk_eval_string(ctx, "40 + 22");
+                         int i = duk_get_int(ctx, -1);
+                         duk_destroy_heap(ctx);
+            return i;
+            }
+            |]
