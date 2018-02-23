68025a7409243658799b7060eaec5d22d799604b
Vikrem <me@vikrem.com>
2018-02-22 19:51:01 -0500

    Testing

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,7 +6,20 @@ module Lib
 
 import qualified Language.C.Inline as C
 
+import qualified Data.ByteString as BS
+import qualified Data.Text as T
+import Foreign.ForeignPtr
+import Foreign.Ptr
+import Control.Concurrent.MVar
+import Data.Monoid
+
+C.context (C.baseCtx <> C.funCtx <> C.fptrCtx <> C.bsCtx)
 C.include "duktape.h"
+
+data DuktapeHeap
+type DuktapeCtx = Ptr DuktapeHeap
+
+data Duk a = MkDuk { unDuk :: DuktapeCtx -> IO a }
 
 someFunc :: IO ()
 someFunc = print =<< [C.block| int {
@@ -17,3 +30,24 @@ someFunc = print =<< [C.block| int {
             return i;
             }
             |]
+cleanup :: Ptr DuktapeHeap -> IO ()
+cleanup ctx = [C.exp| void { duk_destroy_heap($(void* ctx')) }|]
+  where
+   ctx' = castPtr ctx
+
+runDuk :: Duk a -> IO a
+runDuk dk = do
+  ptr <- [C.exp| void* { duk_create_heap_default() } |]
+  (ctx :: ForeignPtr DuktapeHeap) <- castForeignPtr <$> newForeignPtr_ ptr
+  finalizer <- $(C.mkFunPtr [t|Ptr DuktapeHeap -> IO ()|]) cleanup
+  addForeignPtrFinalizer finalizer ctx
+  withForeignPtr ctx $ unDuk dk
+
+
+execJS :: Duk C.CUInt
+execJS = MkDuk $ \ctx ->
+  let ctx' = castPtr ctx in
+  [C.block| unsigned int {
+    duk_eval_string($(void* ctx'), "return 42;");
+    return duk_get_uint($(void* ctx'), -1);
+  }|]
